dbef027681911ec6d56137d56edca689125177ee
franciscolevim <franciscolevim@gmail.com>
2020-05-21 19:23:51 -0500

    Reto 2 - Modularizar el código.

--- src/main.kt
+++ src/main.kt
@@ -16,10 +16,36 @@ fun main(args: Array<String>) {
         print("Selección: ")
         input = readLine()
         when(input) {
-            "1" -> println("Crear")
-            "2" -> println("Ver")
+            "1" -> {
+                viewRecipe("Crear una receta")
+                makeRecipe()
+            }
+            "2" -> viewRecipe("Ver mis recetas")
         }
     } while (input?.compareTo("3") ?: -1 != 0 )
 
     println("Hasta la vista baby")
+}
+
+
+fun makeRecipe() {
+    val menu = """
+        Selecciona por categoría el ingrediente que buscas
+        
+            1. Agua
+            2. leche
+            3. Carne
+            4. Verduras
+            5. Frutas
+            6. Cereal
+            7. Huevos
+            8. Aceites
+            
+    """.trimIndent()
+    println(menu)
+}
+
+
+fun viewRecipe(title: String) {
+    println(":.$title\n")
 }
