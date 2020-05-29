4dc59c9277cd855dda86da923f067d5a2d740256
franciscolevim <franciscolevim@gmail.com>
2020-05-29 12:53:10 -0500

    Modelo Menu.

--- src/main.kt
+++ src/main.kt
@@ -3,11 +3,10 @@ import java.util.*
 
 fun main(args: Array<String>) {
 
-    val recipe = Recipe("Nueva Receta")
-    for (idx in 1..5) {
-        val ingredient = Ingredient("Ingrediente_${idx}", "tazas", 2.5f)
-        recipe.ingredients.add(ingredient)
+    val menu = Menu("Menucito")
+    for (counter in 1..4) {
+        val item = Item("Itemcito_$counter")
+        menu.items.put("$counter", item)
     }
-    recipe.instructions = "Moler todo en la licuadora y después tomar como jugo. ¡Liiiiiistooooooo!"
-    println(recipe)
+    println(menu)
 }
---
+++ src/model/Menu.kt
@@ -0,0 +1,17 @@
+package model
+
+class Menu(title:String): Item(title) {
+
+    val items: HashMap<String, Item> = HashMap<String, Item>()
+
+    override fun toString(): String {
+        var itemsList = ""
+        for (value in items.values) {
+            itemsList += "  ${value.title}\n"
+        }
+        return """
+            *${title}
+            *$itemsList
+        """.trimMargin("*")
+    }
+}
